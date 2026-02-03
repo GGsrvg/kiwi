4080df196272b47ca1c0d8620fc88ed341943964
GGsrvg <samgalsduosy@gmail.com>
2026-02-03 22:42:29 +0700

    commented code returned

--- kiwi/maptype.h
+++ kiwi/maptype.h
@@ -7,6 +7,7 @@
 |----------------------------------------------------------------------------*/
 #pragma once
 #include <functional>
+#include <map>
 #include <memory>
 #include <utility>
 #include "AssocVector.h"
@@ -23,6 +24,13 @@ template <
     typename C = std::less<K>,
     typename A = std::allocator<std::pair<K, V>>>
 using MapType = Loki::AssocVector<K, V, C, A>;
+
+// template<
+// 	typename K,
+// 	typename V,
+// 	typename C = std::less<K>,
+// 	typename A = std::allocator< std::pair<const K, V> > >
+// using MapType = std::map<K, V, C, A>;
 
 } // namespace impl
 
